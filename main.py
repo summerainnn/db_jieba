e3659ab9c16a59eeba0011d46f16a920bbb930a0
summerain <sunbowen@MacBook-Pro.local>
2020-11-12 21:23:59 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import jieba
+
+
+# 创建停用词list
+def stopwordslist(filepath):
+    stopwords = [line.strip() for line in open(filepath, 'r').readlines()]
+    return stopwords
+
+
+# 对句子进行分词
+def seg_sentence(sentence):
+    sentence_seged = jieba.cut(sentence.strip())
+    stopwords = stopwordslist('hit_stopwords.txt')  # 这里加载停用词的路径
+    outstr = ''
+    for word in sentence_seged:
+        if word not in stopwords:
+            if word != '\t':
+                outstr += word
+                outstr += " "
+    return outstr
+
+
+inputs = open('pl.txt', 'r')
+outputs = open('output.txt', 'w')
+for line in inputs:
+    line_seg = seg_sentence(line)  # 这里的返回值是字符串
+    outputs.write(line_seg + '\n')
+outputs.close()
+inputs.close()
